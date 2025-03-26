7a78ff01c39dbdf10ef54176c28d596b74bd8dc3
Oleksandra247 <alexandra.yakymenko@gmail.com>
2025-03-26 19:09:45 +0100

    Update 03_hw.py

--- Lesson05/home_work/02_list/03_hw.py
+++ Lesson05/home_work/02_list/03_hw.py
@@ -2,3 +2,19 @@
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
 # TODO: your code here
+import random
+
+# создаём список из 10 случайных чисел от -100 до 100
+numbers = []
+for i in range(10):
+    num = random.randint(-100, 100)
+    numbers.append(num)
+
+# считаем сумму положительных чисел, которые делятся на 2
+summa = 0
+for number in numbers:
+    if number > 0 and number % 2 == 0:
+        summa += number
+
+print("Список:", numbers)
+print("Сумма положительных чётных чисел:", summa)
