2f54474ee756b92739cc5a557f36b2bd3d465fed
Oleksandra247 <alexandra.yakymenko@gmail.com>
2025-03-27 18:55:41 +0100

    Update 02_hw_dict.py

--- Lesson07/home_work/02_hw_dict.py
+++ Lesson07/home_work/02_hw_dict.py
@@ -29,20 +29,51 @@ staff = [
 # Вычислите:
 print("Имя и Фамилию сотрудника с самой высокой зарплатой:")
 
-# TODO: your code here
+# biggest = staff[0]
+for person in staff:
+    if person["salary"] > biggest["salary"]:
+        biggest = person
+print("Самая высокая зарплата у:", biggest["name"], biggest["surname"])TODO: your code here
 
 print("Имя и Фамилию сотрудника с самой низкой зарплатой:")
 
-# TODO: your code here
+# smallest = staff[0]
+for person in staff:
+    if person["salary"] < smallest["salary"]:
+        smallest = person
+print("Самая низкая зарплата у:", smallest["name"], smallest["surname"])TODO: your code here
 
 print("Среднеарифметическую зарплату всех сотрудников")
 
-# TODO: your code here
+# Ttotal = 0
+for person in staff:
+    total = total + person["salary"]
+average = total / len(staff)
+print("Средняя зарплата:", average)
+ODO: your code here
 
 print("Количество однофамильцев в организации")
 
-# TODO: your code here
+#surnames = []
+for person in staff:
+    surnames.append(person["surname"])
+
+count = 0
+for surname in surnames:
+    if surnames.count(surname) > 1:
+        count += 1
+print("Количество однофамильцев:", count)
+ TODO: your code here
 
 print("*Список всех сотрудников(Имя и Фамилию) в порядке возрастания их зарплаты")
 
-# TODO: your code here
+#for i in range(len(staff)):
+    for j in range(i + 1, len(staff)):
+        if staff[i]["salary"] > staff[j]["salary"]:
+            temp = staff[i]
+            staff[i] = staff[j]
+            staff[j] = temp
+
+print("Сотрудники по зарплате (от меньшей к большей):")
+for person in staff:
+    print(person["name"], person["surname"]) TODO: your code here
