030c7bd1261b1097f8a39eda3a49137891784336
Oleksandra247 <alexandra.yakymenko@gmail.com>
2025-04-22 21:21:57 +0200

    Update 01_hw_func.py

--- Lesson08/home_work/01_hw_func.py
+++ Lesson08/home_work/01_hw_func.py
@@ -3,9 +3,22 @@
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 
 def lucky_ticket(ticket_number):
-    # TODO: your code here
-    pass
+  import math
 
+def dist(x1, y1, x2, y2):
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+x1, y1, r1 = 0, 0, 5
+x2, y2, r2 = 0, 0, 3
+
+distance = dist(x1, y1, x2, y2)
+
+if distance + r2 <= r1:
+    print("Вторая окружность целиком внутри первой")
+elif distance + r1 <= r2:
+    print("Первая окружность целиком внутри второй")
+else:
+    print("Окружности не вложены друг в друга")
 
 # Тестируем функцию
 print(lucky_ticket(123006))
