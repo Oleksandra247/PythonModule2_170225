4aff83adae170531128f9ffb17f3ec0a800e953e
Oleksandra247 <alexandra.yakymenko@gmail.com>
2025-04-24 18:19:11 +0200

    Update 01_hw_modules.py

--- Lesson12/home_work/01_hw_modules.py
+++ Lesson12/home_work/01_hw_modules.py
@@ -1,4 +1,17 @@
-# "Вычисление площади случайного треугольника"
-# Сгенерируйте случайные координаты трех точек на плоскости.
-# Используйте модуль math для вычисления длин сторон треугольника.
-# Вычислите площадь треугольника, используя формулу Герона.
+
+import math
+import random
+
+x1, y1 = random.randint(-10, 10), random.randint(-10, 10)
+x2, y2 = random.randint(-10, 10), random.randint(-10, 10)
+x3, y3 = random.randint(-10, 10), random.randint(-10, 10)
+
+a = math.hypot(x2 - x1, y2 - y1)
+b = math.hypot(x3 - x2, y3 - y2)
+c = math.hypot(x1 - x3, y1 - y3)
+
+p = (a + b + c)/2
+
+s = math.sqrt(p * (p - a) * (p - b) * (p - c))
+
+print(s) "Вычисление площади случайного треугольника"
