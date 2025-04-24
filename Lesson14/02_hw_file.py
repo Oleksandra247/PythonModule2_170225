d814bb88b01e47dcffd007f931ef163454e745dd
Oleksandra247 <alexandra.yakymenko@gmail.com>
2025-04-24 18:27:45 +0200

    Delete Lesson14/02_hw_file.py

--- Lesson14/02_hw_file.py
+++
@@ -1,9 +0,0 @@
-summa = 0
-with open("data/info.txt", "r") as f:
-    for line in f:
-        try:
-            summa += int(line.strip())
-        except ValueError:
-            pass
-
-print(summa)
