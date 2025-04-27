c763d146dcad9460e5489dbbbcaa2393600f1275
Oleksandra247 <alexandra.yakymenko@gmail.com>
2025-04-27 12:35:48 +0200

    Update 02_hw_transform.py

--- Lesson17/home_work/02_hw_transform.py
+++ Lesson17/home_work/02_hw_transform.py
@@ -2,3 +2,10 @@
 # Вам нужно получить список длин всех строк, которые имеют длину более 3 символов.
 
 words = ["cat", "dog", "elephant", "mouse", "bird", "ant"]
+
+lenghts = []
+for i in words:
+    if len(i) > 3:
+        lenghts.append(len(i))
+
+print(lenghts)
